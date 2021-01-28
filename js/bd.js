8137c1c89f5a9b73fd3e1870ad0b1242b7ce5ab5
Jos3GS <56764096+Jos3GS@users.noreply.github.com>
2021-01-28 15:46:47 -0500

    Delete bd.js
    Archivo incompleto

--- js/bd.js
+++
@@ -1,8 +0,0 @@
-var L_Email = document.querySelector(".L_email");
-var L_Pass = document.querySelector(".L_pass");
-
-var R_Name = document.querySelector("R_name");
-var R_Email = document.querySelector("R_email");
-var R_User = document.querySelector("R_user");
-var R_Pass = document.querySelector("R_pass");
-
